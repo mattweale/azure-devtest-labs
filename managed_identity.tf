bd28ea5c9a19726cfd525633cb41c9e6f5aef595
Matt Weale <matt.weale@outlook.com>
2021-09-14 10:26:14 +0100

    Added wsl-setup.txt

--- managed_identity.tf
+++ managed_identity.tf
@@ -6,12 +6,18 @@
 resource "random_uuid" "mi_uid" {
 }
 
+#######################################################################
+## Create MI
+#######################################################################
 resource "azurerm_user_assigned_identity" "dtl_mi" {
   resource_group_name = azurerm_resource_group.rg_dev_test_labs.name
   location            = var.location
   name = "aibBuiUserId-${random_uuid.mi_uid.result}"
 }
 
+#######################################################################
+## Give MI Contributor Role to RG
+#######################################################################
 resource "azurerm_role_assignment" "ra_aib_mi" {
   scope                = azurerm_resource_group.rg_dev_test_labs.id
   role_definition_name = "Contributor"
